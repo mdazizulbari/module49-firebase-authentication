e6d2f0a2fc0820b3e44eea2ec3a2eb5745f32766
smabari <mdazizulbarishafin@gamil.com>
2025-05-11 17:38:19 +0600

    login with github

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,16 @@
-import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import React, { useState } from "react";
 import { auth } from "../firebase/firebase.init";
 
 const Login = () => {
   const [user, setUser] = useState(null);
   const provider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
 
   const handleGoogleSignIn = () => {
     console.log("sign in click");
@@ -12,6 +18,23 @@ const Login = () => {
       .then((result) => {
         console.log(result);
         setUser(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, githubProvider)
+      .then((result) => {
+        const loggedInUser = result.user;
+        setUser(loggedInUser);
+        if (!loggedInUser.email && loggedInUser?.providerData.length) {
+          console.log("user email is not directly provided");
+          if (loggedInUser.providerData[0].email) {
+            loggedInUser.email = loggedInUser.providerData[0].email;
+            setUser(loggedInUser);
+          }
+        }
       })
       .catch((error) => {
         console.log(error);
@@ -35,14 +58,19 @@ const Login = () => {
       {user ? (
         <button onClick={() => handleSignOut()}>Sign Out</button>
       ) : (
-        <button onClick={() => handleGoogleSignIn()}>
-          Sign in with Google
-        </button>
+        <>
+          <button onClick={() => handleGoogleSignIn()}>
+            Sign in with Google
+          </button>
+          <button onClick={() => handleGithubSignIn()}>
+            Sign in with Git Hub
+          </button>
+        </>
       )}
       {user && (
         <div className="">
-          <h3>{user.displayName}</h3>
-          <p>{user.email}</p>
+          <h3>User Name: {user.displayName}</h3>
+          <p>Email: {user.email}</p>
           <img src={user.photoURL} alt="" />
         </div>
       )}
